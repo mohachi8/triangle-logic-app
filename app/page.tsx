1b7169530a2c451c32a5b31ee8cf62f1efbedcb8
mohachi8 <maedoimitsuaki@icloud.com>
2025-05-14 19:30:29 +0900

    依存関係のバージョンを更新し、不要なファイルを削除しました。また、READMEを更新してプロジェクトのタイトルと技術スタックを変更しました。

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
+    <main className="flex flex-col items-center justify-center h-screen">
+      <Button>ボタン</Button>
+    </main>
   );
 }
