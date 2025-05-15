b61c6f1eba67f5460ba31217bb174da5e16f62a1
mohachi8 <maedoimitsuaki@icloud.com>
2025-05-15 16:47:03 +0900

    依存関係にzustandを追加し、react-domのdevオプションを更新しました。また、ページコンポーネントにタイトルとボタンを追加しました。

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/components/ui/button";
 export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <Button>ボタン</Button>
+      <h1 className="text-4xl font-bold">三角ロジック組立演習システム</h1>
+      <Button className="mt-40">学習スタート</Button>
+      <Button className="mt-10">ログイン</Button>
     </main>
   );
 }
